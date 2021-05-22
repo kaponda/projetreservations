50fd8a990a9766717d130c5afa1fd013ca5779cb
kaponda <stefkap10@yahoo.fr>
2021-05-22 09:56:17 +0200

    modif controler type

--- src/Controller/TypesController.php
+++ src/Controller/TypesController.php
@@ -5,7 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use; App\Entity\Types
+use; App\Entity\Types;
 class TypesController extends AbstractController
 {
     /**
