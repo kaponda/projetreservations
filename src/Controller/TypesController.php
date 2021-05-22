e47d9e2a67230a3441b05ce1c16c55209cd2e0bf
kaponda <stefkap10@yahoo.fr>
2021-05-22 09:51:13 +0200

    Models, controllers, templates, routes for Types

---
+++ src/Controller/TypesController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use; App\Entity\Types
+class TypesController extends AbstractController
+{
+    /**
+     * @Route("/types", name="types")
+     */
+    public function index(): Response
+    {
+		$repository = $this->getDoctrine()->getRepository(Types::class);
+        $types = $repository->findAll();
+
+        return $this->render('types/index.html.twig', [
+            'types' => $types,
+            'resource' => 'artistes',
+
+        ]);
+    }
+	    /**
+     * @Route("/type/{id}", name="type_show")
+     */
+    public function show($id)
+    {
+        $repository = $this->getDoctrine()->getRepository(Types::class);
+        $type = $repository->find($id);
+
+        return $this->render('type/show.html.twig', [
+            'type' => $type,
+        ]);
+    }
+
+}
