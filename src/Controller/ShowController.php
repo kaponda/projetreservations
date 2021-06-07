1fc758fae482a6182bdecc6975cbd1be8ee2d666
kaponda <stefkap10@yahoo.fr>
2021-06-07 19:16:36 +0200

    Controller Show +templates +routes

---
+++ src/Controller/ShowController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Show;
+class ShowController extends AbstractController
+{
+    /**
+     * @Route("/show", name="show")
+     */
+    public function index(): Response
+    {
+                $repository = $this->getDoctrine()->getRepository(Show::class);
+        $shows = $repository->findAll();
+        
+        return $this->render('show/index.html.twig', [
+            'shows' => $shows,
+            'resource' => 'spectacles',
+        ]);
+
+    }
+	    /**
+     * @Route("/show/{id}", name="show_show")
+     */
+    public function show($id)
+    {
+        $repository = $this->getDoctrine()->getRepository(Show::class);
+        $show = $repository->find($id);
+
+        return $this->render('show/show.html.twig', [
+            'show' => $show,
+        ]);
+    }
+
+}
