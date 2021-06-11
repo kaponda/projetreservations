0bb2fbac353cb661bc2ba0b83568491fa9696637
kaponda <stefkap10@yahoo.fr>
2021-06-11 12:17:37 +0200

    Relation ManyToMany between Artist & Type 44

--- src/Entity/Artists.php
+++ src/Entity/Artists.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\ArtistsRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -27,6 +29,16 @@ class Artists
      * @ORM\Column(type="string", length=60)
      */
     private $lastname;
+
+    /**
+     * @ORM\ManyToMany(targetEntity=Types::class, inversedBy="artists")
+     */
+    private $types;
+
+    public function __construct()
+    {
+        $this->types = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -53,6 +65,33 @@ class Artists
     public function setLastname(string $lastname): self
     {
         $this->lastname = $lastname;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Types[]
+     */
+    public function getTypes(): Collection
+    {
+        return $this->types;
+    }
+
+    public function addType(Types $type): self
+    {
+        if (!$this->types->contains($type)) {
+            $this->types[] = $type;
+        }
+
+        return $this;
+    }
+
+    public function removeType(Types $type): self
+    {
+                if ($this->types->contains($type)) {
+            $this->types->removeElement($type);
+        }
+
 
         return $this;
     }
--- src/Entity/Types.php
+++ src/Entity/Types.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\TypesRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -23,6 +25,16 @@ class Types
      */
     private $type;
 
+    /**
+     * @ORM\ManyToMany(targetEntity=Artists::class, mappedBy="types")
+     */
+    private $artists;
+
+    public function __construct()
+    {
+        $this->artists = new ArrayCollection();
+    }
+
     public function getId(): ?int
     {
         return $this->id;
@@ -36,6 +48,35 @@ class Types
     public function setType(string $type): self
     {
         $this->type = $type;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Artists[]
+     */
+    public function getArtists(): Collection
+    {
+        return $this->artists;
+    }
+
+    public function addArtist(Artists $artist): self
+    {
+        if (!$this->artists->contains($artist)) {
+            $this->artists[] = $artist;
+            $artist->addType($this);
+        }
+
+        return $this;
+    }
+
+    public function removeArtist(Artists $artist): self
+    {
+                if ($this->artists->contains($artist)) {
+            $this->artists->removeElement($artist);
+            $artist->removeType($this);
+        }
+
 
         return $this;
     }
